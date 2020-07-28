657f850d25519be7a84af0a906d7d5b63d8bcde7
Nicholas <nicholasnadel@gmail.com>
2020-07-28 12:14:35 -0700

    One column responsive tweaks

--- app/assets/javascripts/cascade/level/cu-safely-back.js
+++ app/assets/javascripts/cascade/level/cu-safely-back.js
@@ -1,6 +1,7 @@
 $(window).on("load resize", function (e) {
   if ($(".one-column-template .text-with-cta-widget--default").length) {
-    if ($(window).width() > 768) {
+    console.log('chaning --default text with cta')
+    if ($(window).width() > 905) {
       $('.one-column-template .text-with-cta-widget--default').each(function () {
         var buttons = $(this).find('.text-with-cta__buttons')
         var title = $(this).find('.text-with-cta__title')
