3e0793271352e80209e7fa97697788d3630a2ea6
Nicholas <nicholasnadel@gmail.com>
2020-07-28 11:51:10 -0700

    Change one-column-template --default markup with JS

--- app/assets/javascripts/cascade/level/cu-safely-back.js
+++ app/assets/javascripts/cascade/level/cu-safely-back.js
@@ -1,9 +1,20 @@
-$(function () {
+$(window).on("load resize", function (e) {
   if ($(".one-column-template .text-with-cta-widget--default").length) {
-    $('.one-column-template .text-with-cta-widget--default').each(function () {
-      var buttons = $(this).find('.text-with-cta__buttons')
-      var title = $(this).find('.text-with-cta__title')
-      buttons.detach().appendTo(title)
-    })
+    if ($(window).width() > 768) {
+      $('.one-column-template .text-with-cta-widget--default').each(function () {
+        var buttons = $(this).find('.text-with-cta__buttons')
+        var title = $(this).find('.text-with-cta__title')
+        var text = $(this).find('.text-with-cta__text')
+        buttons.detach().appendTo(title)
+      })
+    }
+    else {
+      $('.one-column-template .text-with-cta-widget--default').each(function () {
+        var buttons = $(this).find('.text-with-cta__buttons')
+        var title = $(this).find('.text-with-cta__title')
+        var text = $(this).find('.text-with-cta__text')
+        buttons.detach().appendTo(text)
+      })
+    }
   }
 });
