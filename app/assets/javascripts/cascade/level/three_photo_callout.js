4ee17db0d8ff0c8aa972bd42d436ded7cc437c04
Nicholas <nicholasnadel@gmail.com>
2020-09-16 21:02:08 -0700

    revamp each photo block width

--- app/assets/javascripts/cascade/level/three_photo_callout.js
+++ app/assets/javascripts/cascade/level/three_photo_callout.js
@@ -98,6 +98,7 @@ $(document).ready(function () {
         });
     });
     objectFitFallBackForIe();
+    // normalizeImageHeights();
 });
 function resetbuttonClickCounterer() {
     var buttonClickCounter = 0;
