d9067528907c500ba82757732c815c34fb1217e7
Nicholas <nicholasnadel@gmail.com>
2020-09-17 17:51:35 -0700

    beginning image-slider

--- app/assets/javascripts/widgets/image_slider.js
+++ app/assets/javascripts/widgets/image_slider.js
@@ -36,4 +36,99 @@ $(document).ready(function () {
       t.hide();
     });
   }
+
+
+  clickPause();
+  clickPlay();
+
+
+  $('.cd-hero-pause').keydown(function (e) {
+    if (e.which == 13) {//Enter key pressed
+      pause()
+    }
+  });
+
+  $('.cd-hero-play').keypress(function (e) {
+    if (e.which == 13) {//Enter key pressed
+      play();
+    }
+  });
+
+  function clickPause() {
+    $('.cd-hero-pause').on('click', function (event) {
+      pause();
+    });
+  }
+
+  function pause() {
+
+    $('.cd-hero-pause').hide();
+    $('.cd-hero-play').show();
+
+    // $('video').trigger($('video').prop('paused') ? 'play' : 'pause');
+
+    // $('.brochure-masthead *').removeClass('autoplay').addClass('masthead-paused')
+
+    $('.brochure-masthead * ').filter('.autoplay').addClass('masthead-paused').removeClass('.autoplay');
+
+    $(".widget-slides").lightSlider({
+      auto: false,
+    });
+
+
+
+
+    if ($('video').length) {
+      var vid = $("video");
+      if ($(vid).get(0).paused) {
+        vid.removeClass('homepage-masthead__play-video--paused')
+        $('#homepage-masthead__play-button').hide();
+        $('#homepage-masthead__pause-button').show();
+        $(vid).trigger('play');
+      } else {
+        vid.addClass('homepage-masthead__video--paused')
+        $('#homepage-masthead__pause-button').hide();
+        $('#homepage-masthead__play-button').show();
+        $(vid).trigger('pause');
+      }
+    }
+  }
+  function clickPlay() {
+
+    $('.cd-hero-play').on('click', function (event) {
+      play();
+
+    });
+  }
+
+  function play() {
+    $('.cd-hero-play').hide();
+    $('.cd-hero-pause').show();
+    // $('video').trigger($('video').prop('paused') ? 'play' : 'pause');
+
+    // $('.brochure-masthead *').removeClass('autoplay').addClass('masthead-paused')
+
+    $('.brochure-masthead').each(function () {
+      if ($(this).hasClass('masthead-paused'))
+        $(this).addClass('autoplay').removeClass('masthead-pausd')
+    })
+    if ($('video').length) {
+      var vid = $("video");
+      if ($(vid).get(0).paused) {
+        vid.removeClass('homepage-masthead__play-video--paused')
+        $('#homepage-masthead__play-button').hide();
+        $('#homepage-masthead__pause-button').show();
+        $(vid).trigger('play');
+      } else {
+        vid.addClass('homepage-masthead__video--paused')
+        $('#homepage-masthead__pause-button').hide();
+        $('#homepage-masthead__play-button').show();
+        $(vid).trigger('pause');
+      }
+    }
+    $(".widget-slides").lightSlider({
+      auto: false,
+    });
+
+  }
 });
