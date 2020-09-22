9262944eb2cba392f5a3ce707ea2778752c8830c
Nicholas <nicholasnadel@gmail.com>
2020-09-22 13:40:04 -0700

    Image Slider - make plugin's active state somewhat more reliable

--- app/assets/javascripts/widgets/image_slider.js
+++ app/assets/javascripts/widgets/image_slider.js
@@ -1,8 +1,5 @@
 $(document).ready(function () {
   if ($('.image-slider')[0]) {
-
-
-
     var slider = $(".widget-slides").lightSlider({
       gallery: false,
       item: 1,
@@ -25,75 +22,20 @@ $(document).ready(function () {
     // $('.image-slider-play-toggle').prepend('.lSPager')
     // $('.lSPager').append('.image-slider-play-toggle')
     // $('.image-slider-play-toggle').insertBefore('ul.lSpg li:first')
-
     // slider.pause()
-
-
-
-
-
     clickPause();
     clickPlay();
-
-
+    fixActiveState();
     $('.image-slider-pause').keydown(function (e) {
-      if (e.which == 13) {//Enter key pressed
+      if (e.which == 13) { //Enter key pressed
         pause();
       }
     });
-
     $('.image-slider-play').keypress(function (e) {
-      if (e.which == 13) {//Enter key pressed
+      if (e.which == 13) { //Enter key pressed
         play();
       }
     });
-
-    function clickPause() {
-      $('.image-slider-pause').on('click', function (event) {
-
-        pause();
-
-      });
-    }
-
-    function pause() {
-      $('.image-slider-pause').hide();
-      $('.image-slider-play').show();
-
-
-      // $(".lSSlideWrapper").addClass('ls-hover')
-
-      // console.log(slider)
-      slider.pause();
-
-
-    }
-    function clickPlay() {
-      $('.image-slider-play').on('click', function (event) {
-
-        play();
-
-
-      });
-    }
-
-    function play() {
-      $('.image-slider-play').hide();
-      $('.image-slider-pause').show();
-      // $('video').trigger($('video').prop('paused') ? 'play' : 'pause');
-
-      slider.play();
-      // $(".lSSlideWrapper").removeClass('ls-hover')
-
-
-    }
-
-
-
-
-
-
-
     // IE Fallback for widget-slides img { object-fit: } CSS property 
     if (document.documentMode || /Edge/.test(navigator.userAgent)) {
       $('img').each(function () {
@@ -101,7 +43,6 @@ $(document).ready(function () {
           s = 'url(' + t.attr('src') + ')',
           p = t.parent(),
           d = $('<div></div>');
-
         p.append(d);
         d.css({
           'height': t.parent().css('height'),
@@ -113,21 +54,42 @@ $(document).ready(function () {
         t.hide();
       });
     }
+    function clickPause() {
+      $('.image-slider-pause').on('click', function (event) {
+        pause();
+      });
+    }
+    function pause() {
+      $('.image-slider-pause').hide();
+      $('.image-slider-play').show();
+      slider.pause();
+    }
+    function clickPlay() {
+      $('.image-slider-play').on('click', function (event) {
+        play();
+      });
+    }
+    function play() {
+      $('.image-slider-play').hide();
+      $('.image-slider-pause').show();
+      // $('video').trigger($('video').prop('paused') ? 'play' : 'pause');
+      slider.play();
+      // $(".lSSlideWrapper").removeClass('ls-hover')
+    }
   }
-
 });
-
 $(window).load(function () {
   if ($('.image-slider-play-toggle')[0]) {
-    $('.image-slider-play-toggle').insertBefore('ul.lSpg li:first')
-
+    $('.image-slider-play-toggle').insertBefore('ul.lSpg li:first');
+    fixActiveState();
   }
-
 });
-
-
-
-
-
-
-
+function fixActiveState() {
+  // fix plugin's 'active' class inconsitency
+  $('.lSPager li').on('click', function (event) {
+    $('.lSPager *').removeClass('cu-active');
+    $('.lSPager *').removeClass('active');
+    $(this).find('a').addClass('cu-active');
+    $(this).find('a').addClass('active');
+  });
+}
