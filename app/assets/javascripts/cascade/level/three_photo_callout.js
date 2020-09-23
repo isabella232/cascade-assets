210a9baba6abfc27d906f40fd02b46e4ddd1781b
Nicholas <nicholasnadel@gmail.com>
2020-09-23 11:37:33 -0700

    check for class before firing

--- app/assets/javascripts/cascade/level/three_photo_callout.js
+++ app/assets/javascripts/cascade/level/three_photo_callout.js
@@ -1,78 +1,80 @@
 $(document).ready(function () {
-    $.each($('.photo-callout-widget__button'), function (ind) {
-        $(this).attr('id', 'photo-callout-widget__button__' + parseInt(ind + 1));
-    })
-    $.each($('.photo-callout-widget__container'), function (ind) {
-        if ($(this).find(' .photo-callout-widget:hidden').size() <= 0) {
-            $(this).find('.photo-callout-widget__button').fadeOut()
-        }
-        $(this).attr('id', 'photo-callout-widget__container__' + parseInt(ind + 1));
-        var currentWidgetContainer = $(this).closest('.photo-callout-widget__container').attr('id');
-        var currentTotalNumberOfPhotos = $('#' + currentWidgetContainer + " .photo-callout-widget").size()
-        var currentHidden = $('#' + currentWidgetContainer + ' .photo-callout-widget:hidden').size();
-        var loadMoreButtonButton = ' + .photo-callout-widget__button';
-        var currentButton = '#' + currentWidgetContainer + loadMoreButtonButton;
-        var numberOfPhotoDivsToReveal = 3;
-        var photoIncrement = 2;
-        var numDivsToShow = 2;
-        if ($('#' + currentWidgetContainer).hasClass('photo-callout-widget__container--2-col')) {
+    if ($('.photo-callout-widget__container')[0]) {
+        $.each($('.photo-callout-widget__button'), function (ind) {
+            $(this).attr('id', 'photo-callout-widget__button__' + parseInt(ind + 1));
+        })
+        $.each($('.photo-callout-widget__container'), function (ind) {
+            if ($(this).find(' .photo-callout-widget:hidden').size() <= 0) {
+                $(this).find('.photo-callout-widget__button').fadeOut()
+            }
+            $(this).attr('id', 'photo-callout-widget__container__' + parseInt(ind + 1));
+            var currentWidgetContainer = $(this).closest('.photo-callout-widget__container').attr('id');
+            var currentTotalNumberOfPhotos = $('#' + currentWidgetContainer + " .photo-callout-widget").size()
+            var currentHidden = $('#' + currentWidgetContainer + ' .photo-callout-widget:hidden').size();
+            var loadMoreButtonButton = ' + .photo-callout-widget__button';
+            var currentButton = '#' + currentWidgetContainer + loadMoreButtonButton;
+            var numberOfPhotoDivsToReveal = 3;
             var photoIncrement = 2;
-            var numDivsToShow = 4;
-        } else if ($('#' + currentWidgetContainer).hasClass('photo-callout-widget__container--3-col')) {
-            var photoIncrement = 3;
-            var numDivsToShow = 6;
-        } else if ($('#' + currentWidgetContainer).hasClass('photo-callout-widget__container--4-col')) {
-            var photoIncrement = 8;
-            var numDivsToShow = 8;
-        } else if ($('#' + currentWidgetContainer).hasClass('photo-callout-widget__container--5-col')) {
-            var photoIncrement = 5;
-            var numDivsToShow = 10;
-        } else if ($('#' + currentWidgetContainer).hasClass('photo-callout-widget__container--6-col')) {
-            var photoIncrement = 6;
-            var numDivsToShow = 12;
-        }
-        var currentVisible = $('#' + currentWidgetContainer + ' img:visible').length;
-        // var currentVisible = $('#' + currentWidgetContainer + ' .photo-callout-widget:visible').size()
-        // Hide image wrapper divs to start
-        $('#' + currentWidgetContainer + ' > .photo-callout-widget').hide();
-        // Show the starting number specified above
-        $('#' + currentWidgetContainer + ' > .photo-callout-widget:lt(' + numDivsToShow + ')').show();
-        var buttonClickCounter = 0;
-        $('button.photo-callout-widget__button--paginate').show();
-        var hiddenItems = $(this).find('.photo-callout-widget:hidden');
-        var button = $(this).next('.photo-callout-widget__button')
-        if (hiddenItems.length <= 0) {
-            $(button).fadeOut()
-        }
-        $(currentButton).click(function () {
+            var numDivsToShow = 2;
+            if ($('#' + currentWidgetContainer).hasClass('photo-callout-widget__container--2-col')) {
+                var photoIncrement = 2;
+                var numDivsToShow = 4;
+            } else if ($('#' + currentWidgetContainer).hasClass('photo-callout-widget__container--3-col')) {
+                var photoIncrement = 3;
+                var numDivsToShow = 6;
+            } else if ($('#' + currentWidgetContainer).hasClass('photo-callout-widget__container--4-col')) {
+                var photoIncrement = 8;
+                var numDivsToShow = 8;
+            } else if ($('#' + currentWidgetContainer).hasClass('photo-callout-widget__container--5-col')) {
+                var photoIncrement = 5;
+                var numDivsToShow = 10;
+            } else if ($('#' + currentWidgetContainer).hasClass('photo-callout-widget__container--6-col')) {
+                var photoIncrement = 6;
+                var numDivsToShow = 12;
+            }
             var currentVisible = $('#' + currentWidgetContainer + ' img:visible').length;
-            buttonClickCounter += 1;
-            numDivsToShow = (numDivsToShow + 6);
-            $('#' + currentWidgetContainer + ' > a:lt(' + numDivsToShow + ')').show();
-            if (buttonClickCounter < 2) {
-                $('#' + currentWidgetContainer + ' > a:lt(' + numDivsToShow + ')').show();
-                $('#' + currentWidgetContainer + ' > div:lt(' + numDivsToShow + ')').show();
-            } else if (buttonClickCounter == 2 && currentTotalNumberOfPhotos > 6) {
-                $(currentButton).text('Load All')
-                $('#' + currentWidgetContainer + ' > a:lt(' + numDivsToShow + ')').show();
-                $('#' + currentWidgetContainer + ' > div:lt(' + numDivsToShow + ')').show();
-            } else if (buttonClickCounter > 2) {
-                $('#' + currentWidgetContainer + ' > a').show(0);
-                $('#' + currentWidgetContainer + ' > div').show(0);
-                $(currentButton).text('All Photos Loaded')
-                $(currentButton).fadeOut(0);
+            // var currentVisible = $('#' + currentWidgetContainer + ' .photo-callout-widget:visible').size()
+            // Hide image wrapper divs to start
+            $('#' + currentWidgetContainer + ' > .photo-callout-widget').hide();
+            // Show the starting number specified above
+            $('#' + currentWidgetContainer + ' > .photo-callout-widget:lt(' + numDivsToShow + ')').show();
+            var buttonClickCounter = 0;
+            $('button.photo-callout-widget__button--paginate').show();
+            var hiddenItems = $(this).find('.photo-callout-widget:hidden');
+            var button = $(this).next('.photo-callout-widget__button')
+            if (hiddenItems.length <= 0) {
+                $(button).fadeOut()
             }
-            var currentVisible = $('#' + currentWidgetContainer + ' .photo-callout-widget:visible').size()
-            if (currentVisible == currentTotalNumberOfPhotos) {
-                $(currentButton).text('All Photos Loaded')
-                $(currentButton).fadeOut(0);
-            }
-            // end click function
+            $(currentButton).click(function () {
+                var currentVisible = $('#' + currentWidgetContainer + ' img:visible').length;
+                buttonClickCounter += 1;
+                numDivsToShow = (numDivsToShow + 6);
+                $('#' + currentWidgetContainer + ' > a:lt(' + numDivsToShow + ')').show();
+                if (buttonClickCounter < 2) {
+                    $('#' + currentWidgetContainer + ' > a:lt(' + numDivsToShow + ')').show();
+                    $('#' + currentWidgetContainer + ' > div:lt(' + numDivsToShow + ')').show();
+                } else if (buttonClickCounter == 2 && currentTotalNumberOfPhotos > 6) {
+                    $(currentButton).text('Load All')
+                    $('#' + currentWidgetContainer + ' > a:lt(' + numDivsToShow + ')').show();
+                    $('#' + currentWidgetContainer + ' > div:lt(' + numDivsToShow + ')').show();
+                } else if (buttonClickCounter > 2) {
+                    $('#' + currentWidgetContainer + ' > a').show(0);
+                    $('#' + currentWidgetContainer + ' > div').show(0);
+                    $(currentButton).text('All Photos Loaded')
+                    $(currentButton).fadeOut(0);
+                }
+                var currentVisible = $('#' + currentWidgetContainer + ' .photo-callout-widget:visible').size()
+                if (currentVisible == currentTotalNumberOfPhotos) {
+                    $(currentButton).text('All Photos Loaded')
+                    $(currentButton).fadeOut(0);
+                }
+                // end click function
+            });
+            var currentHidden = $('#' + currentWidgetContainer + ' .photo-callout-widget:hidden').length;
+            // end each widget function
         });
-        var currentHidden = $('#' + currentWidgetContainer + ' .photo-callout-widget:hidden').length;
-        // end each widget function
-    });
-    objectFitFallBackForIe();
+        objectFitFallBackForIe();
+    }
 });
 
 function resetbuttonClickCounterer() {
