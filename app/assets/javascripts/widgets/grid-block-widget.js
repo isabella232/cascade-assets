9fd4f5b82116612ea31f127d72e11130788e046c
Nicholas Nadel <nicholasnadel@gmail.com>
2021-03-08 15:48:53 -0800

    ensure global function is available

--- app/assets/javascripts/widgets/grid-block-widget.js
+++ app/assets/javascripts/widgets/grid-block-widget.js
@@ -41,6 +41,23 @@ $(function () {
   }
 });
 
+var accessibleClick = function (event) {
+  var code = event.charCode || event.keyCode,
+    type = event.type;
+
+  if (type === "click") {
+    console.log("click");
+    return true;
+  } else if (type === "keydown") {
+    if (code === 32 || code === 13) {
+      event.preventDefault();
+      return true;
+    }
+  } else {
+    return false;
+  }
+};
+
 function calculateDataHeight() {
   // 2 & 3 COLUMN
   $(
