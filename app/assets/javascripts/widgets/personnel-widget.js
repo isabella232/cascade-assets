460059e1acd4333f9ceda6f6988e25301ad456c8
Nicholas Nadel <nicholasnadel@gmail.com>
2021-03-16 20:16:52 -0700

    progress

---
+++ app/assets/javascripts/widgets/personnel-widget.js
@@ -0,0 +1,23 @@
+$(function () {
+  if ($(".personnel-widget__container").length) {
+    normalizeHeights();
+  }
+
+  function normalizeHeights() {
+    // Normalizes height discrepancies on grid block
+    $(".personnel-widget__container").each(function () {
+      // Get an array of all element heights
+      var elementHeights = $(this)
+        .find(".personnel-widget__title")
+        .map(function () {
+          return $(this).height();
+        })
+        .get();
+      // Math.max takes a variable number of arguments
+      // `apply` is equivalent to passing each height as an argument
+      var tallest = Math.max.apply(null, elementHeights);
+      // Set each height to the max height
+      $(this).find(".personnel-widget__title").css("min-height", tallest);
+    });
+  }
+});
