5c71afe6d15a5886ab9a394a7eceb1c9a02b5825
nn <nicholasnadel@gmail.com>
2021-08-17 15:18:29 -0700

    UI Progress; set wavy bg height height with JS

--- subprojects/degrees-and-programs/source/static/app/js/degreesAndPrograms.js
+++ subprojects/degrees-and-programs/source/static/app/js/degreesAndPrograms.js
@@ -6,63 +6,80 @@ var chapman = chapman || {};
 
 (function ($, Modernizr, window, document) {
 
-	'use strict';
+  'use strict';
 
-	chapman.degreesAndPrograms = {
+  chapman.degreesAndPrograms = {
 
-		init: function () {
+    init: function () {
 
-			$('.degrees-and-programs').addClass('ready');
+      $('.degrees-and-programs').addClass('ready');
 
-			this.bindUIEvents();
+      this.bindUIEvents();
 
-		},
 
-		bindUIEvents: function () {
+    },
 
-			$('.results-views li').on('click', function () {
-				chapman.degreesAndPrograms.switchResultsView($(this));
-			});
+    bindUIEvents: function () {
 
-			$('.dap-results').on('click', '.active-content-toggle', function (event) {
-				event.preventDefault();
-				$(this).closest('.result').toggleClass('active');
-			});
+      $('.results-views li').on('click', function () {
+        chapman.degreesAndPrograms.switchResultsView($(this));
+      });
 
-			$('#js-dap-undergraduate-interests .show-more a').on('click', function () {
-				$('#js-dap-undergraduate-interests-list').addClass('show-all');
-			});
+      $('.dap-results').on('click', '.active-content-toggle', function (event) {
+        event.preventDefault();
+        $(this).closest('.result').toggleClass('active');
+      });
 
-		},
+      $('#js-dap-undergraduate-interests .show-more a').on('click', function () {
+        $('#js-dap-undergraduate-interests-list').addClass('show-all');
+      });
 
-		switchResultsView: function (el) {
-			var viewButton = el,
-				newView = viewButton.data('view'),
-				results = viewButton.closest('.dap-results');
+    },
 
-			if (!(viewButton.hasClass('active'))) {
+    switchResultsView: function (el) {
+      var viewButton = el,
+        newView = viewButton.data('view'),
+        results = viewButton.closest('.dap-results');
 
-				viewButton.closest('.results-views').find('li').removeClass('active');
-				viewButton.addClass('active');
+      if (!(viewButton.hasClass('active'))) {
 
-				if (newView === 'grid-view') {
-					results.removeClass('list-view').addClass('grid-view');
-				} else if (newView === 'list-view') {
-					results.removeClass('grid-view').addClass('list-view');
-				}
+        viewButton.closest('.results-views').find('li').removeClass('active');
+        viewButton.addClass('active');
 
-			}
+        if (newView === 'grid-view') {
+          results.removeClass('list-view').addClass('grid-view');
+        } else if (newView === 'list-view') {
+          results.removeClass('grid-view').addClass('list-view');
+        }
 
-		}
+      }
 
-	};
+    }
+
+  };
 
 })(window.jQuery, window.Modernizr, window, window.document);
 
 $(function () {
 
-	'use strict';
+  'use strict';
 
-	chapman.degreesAndPrograms.init();
+  chapman.degreesAndPrograms.init();
+
 
 });
+
+$(window).load(function () {
+  setWavyBgHeight();
+});
+
+function setWavyBgHeight() {
+  $('form').each(function () {
+    var contentHeight = $(this).outerHeight()
+    var wavyBGHeight = $(this).closest('.wavy-bg').height()
+    //     var wavyBGHeight = window.getComputedStyle(wavyBG, ':before').height; // Returns (string) "70px"
+
+    $(this).find('.wavy-bg').height(contentHeight)
+
+  })
+}
