2ea227620464a4bf6b29a5af4648a92769e93fa3
nn <nicholasnadel@gmail.com>
2021-08-18 13:24:47 -0700

    make program-info close button accessible

--- subprojects/degrees-and-programs/source/static/app/js/degreesAndPrograms.js
+++ subprojects/degrees-and-programs/source/static/app/js/degreesAndPrograms.js
@@ -1,96 +1,66 @@
 /*global window:false */
 /*global $:false */
 /*jslint plusplus: true */
-
 var chapman = chapman || {};
-
 (function ($, Modernizr, window, document) {
-
   'use strict';
-
   chapman.degreesAndPrograms = {
-
     init: function () {
-
       $('.degrees-and-programs').addClass('ready');
-
       this.bindUIEvents();
-
     },
-
     bindUIEvents: function () {
-
       $('.results-views li').on('click', function () {
         chapman.degreesAndPrograms.switchResultsView($(this));
       });
-
       $('.dap-results').on('click', '.active-content-toggle', function (event) {
         event.preventDefault();
         $(this).closest('.result').toggleClass('active');
         $(this).closest('article').siblings().removeClass('active');
+        $('.program-info__close').on('click', function (event) {
+          $(this).closest('article.active').removeClass('active');
+        });
+        $('.program-info__close').on('click keypress', function (event) {
+          event.preventDefault();
+          if (a11yClick(event) === true) {
+            $(this).closest('article.active').removeClass('active');
+          }
+        });
+
+        function a11yClick(event) {
+          if (event.type === 'click') {
+            return true;
+          } else if (event.type === 'keypress') {
+            var code = event.charCode || event.keyCode;
+            if ((code === 32) || (code === 13)) {
+              return true;
+            }
+          } else {
+            return false;
+          }
+        }
       });
-
-       $('svg.program-info__close').on('click', function (event) {
-         alert('click')
-        $(this).closest('article.active').removeClass('active');
-      });
-
-
-
       $('#js-dap-undergraduate-interests .show-more a').on('click', function () {
         $('#js-dap-undergraduate-interests-list').addClass('show-all');
       });
-
-
-
-
     },
-
-
     switchResultsView: function (el) {
       var viewButton = el,
         newView = viewButton.data('view'),
         results = viewButton.closest('.dap-results');
-
       if (!(viewButton.hasClass('active'))) {
-
         viewButton.closest('.results-views').find('li').removeClass('active');
         viewButton.addClass('active');
-
         if (newView === 'grid-view') {
           results.removeClass('list-view').addClass('grid-view');
         } else if (newView === 'list-view') {
           results.removeClass('grid-view').addClass('list-view');
         }
-
       }
-
     }
-
   };
-
-
-// USAGE:
-// $('#fake-button').on('click keypress', function(event){
-//   if(a11yClick(event) === true){
-//     // do stuff
-//   }
-// });
-
-
-
-
-
 })(window.jQuery, window.Modernizr, window, window.document);
-
 $(function () {
-
   'use strict';
-
   chapman.degreesAndPrograms.init();
-
-
-  
-
 });
-
